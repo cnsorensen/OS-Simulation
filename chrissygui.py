813cdff936c86d117eb7ffbf6ecc45f7b474df17
cnsorensen <christine.sorensen@mines.sdsmt.edu>
2016-04-27 16:04:05 -0600

    I renamed my part. I'll work on the page replacement tonight.

--- chrissygui.py
+++ chrissygui.py
@@ -5,26 +5,31 @@ import random
 def test():
     print 'Hello mother fucking world!!!'
 
+def pr_generate():
+    print 'Page Replacement in da hizzhowwwss'
+    canline = pr_canvas.create_line( 0, 0, 200, 100 )
+    canbox = pr_canvas.create_rectangle( ( 150, 150, 250, 250 ) )
+
 def printvar( var ):
     print var[0].get()
 
 def pushvar( var ):
-	for i in range( N-1, 0, -1 ):
-		var[i].set( var[i-1].get() )
-	var[0].set( '' )
+    for i in range( N-1, 0, -1 ):
+        var[i].set( var[i-1].get() )
+    var[0].set( '' )
 
 # Creates a line
 def line():
-	canline = can.create_line( 0, 0, 200, 100 )
-	canbox = can.create_rectangle( ( 150, 150, 250, 250 ) )
-	bcan.config( text = "Hide", command = hide )
+    canline = can.create_line( 0, 0, 250, 250, 53, 69, 300, 300 )
+    canbox = can.create_rectangle( ( 150, 150, 250, 250 ) )
+    bcan.config( text = "Hide", command = hide )
 
 def hide():
-	bcan.config( text = "Draw", command = line )
-	can.delete( "all" )
+    bcan.config( text = "Draw", command = line )
+    can.delete( "all" )
 
 def randvar():
-	string2[0].set( random.randint( 0, 32 ) )
+    string2[0].set( random.randint( 0, 32 ) )
 
 # Sets up the GUI
 def gui():
@@ -50,7 +55,7 @@ def gui():
     return root, one, two, three
 
 # Process Scheduler tab
-def frame1():	
+def frame1():   
     L = Label( one, text = 'Im a labelly label' )
     L.pack()
     b = Button( one, text = "OK", command = exit )
@@ -104,36 +109,21 @@ def frame3():
         pr_alg_b = Radiobutton( top, text = text, variable = pr_alg, value = mode )
         pr_alg_b.pack( side = LEFT )
 
+    # get the number of frames from the user
+    num_frames = StringVar()
+    num_frames_e = Entry( three, width = 20, textvariable = num_frames )
+    num_frames_e.pack()
+
+
     # generate button to submit the page replacement information
-    generate_b = Button( three, text = "Generate", command = test )
-    generate_b.pack()
+    pr_generate_b = Button( three, text = "Generate", command = pr_generate )
+    pr_generate_b.pack()
 
+    # the canvas where the page replacement results will be laid
+    pr_canvas = Canvas( three, width = 500, height = 500 )
+    pr_canvas.pack()
 
-    '''
-    string1 = []
-    entry1 = []
-    for i in range( M ): 
-	    string1.append( StringVar() )
-	    entry1.append( Entry( top, width = 5, textvariable = string1[i] ) )
-	    entry1[i].pack( side = LEFT )
-
-    string2 = []
-    entry2 = []
-    for i in range( N ): 
-	    string2.append( StringVar() )
-	    entry2.append( Entry( bottom, width = 6, textvariable = string2[i] ) )
-	    entry2[i].pack()
-
-    b1 = Button( cmds, text = "Push", command = lambda: pushvar( string2 ) )
-    b1.pack( side = LEFT )
-
-    b2 = Button( cmds, text = "Print", command = lambda: printvar( string2 ) )
-    b2.pack( side = LEFT )
-
-    b3 = Button( cmds, text = "Random", command = randvar )
-    b3.pack( side = LEFT )
-    var = string1[0].get()'''
-    return string1, string2
+    return pr_generate_b, pr_canvas
 
 if __name__ == "__main__":
     N = 5
@@ -142,6 +132,7 @@ if __name__ == "__main__":
     root,one,two,three = gui()
     frame1()
     bcan, can = frame2()
-    string1, string2 = frame3()
+    pr_generate_b, pr_canvas = frame3()
 
     root.mainloop()
+
