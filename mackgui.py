4e7c7f0bd0eac04ff621219a130ac3ab097ec277
Smith <7057627@linux03.mcs.sdsmt.edu>
2016-05-01 17:12:26 -0600

    scheduling is almost done, jsut need to figure out how to make the gantt charts correctly for the different algorithms

--- mackgui.py
+++ mackgui.py
@@ -2,8 +2,14 @@ from Tkinter import *
 from ttk import *
 import random
 
+
 def test():
     print 'Hello mother fucking world!!!'
+
+def pr_generate():
+    print 'Page Replacement in da hizzhowwwss'
+    canline = pr_canvas.create_line( 0, 0, 200, 100 )
+    canbox = pr_canvas.create_rectangle( ( 150, 150, 250, 250 ) )
 
 def printvar( var ):
     print var[0].get()
@@ -15,7 +21,7 @@ def pushvar( var ):
 
 # Creates a line
 def line():
-	canline = can.create_line( 0, 0, 200, 100 )
+	canline = can.create_line( 0, 0, 250, 250, 53, 69, 300, 300 )
 	canbox = can.create_rectangle( ( 150, 150, 250, 250 ) )
 	bcan.config( text = "Hide", command = hide )
 
@@ -33,7 +39,7 @@ def gui():
     # title of the window
     root.title( "Simulation Project" )
     # set the size of the window
-    root.geometry( "500x300" )
+    root.geometry( "1000x1000" )
     # set up the tab structure
     notebook = Notebook( root )
 
@@ -42,25 +48,77 @@ def gui():
     two = Frame( notebook )
     three = Frame( notebook ) 
 
-    notebook.add( one, text = "Procss Scheduler" )
+    notebook.add( one, text = "Process Scheduler" )
     notebook.add( two, text = "Memory Management Unit" )
     notebook.add( three, text = "Page Replacement" )
     notebook.pack( side = TOP )
     random.seed()
     return root, one, two, three
 
+
+
 # Process Scheduler tab
-def frame1():	
-    L = Label( one, text = 'Im a labelly label' )
-    L.pack()
-    b = Button( one, text = "OK", command = exit )
+def frame1(frame):	
+    top = Frame(one)
+    left = Frame(one)
+    left.pack(side=LEFT)
+    top.pack(side=TOP)
+
+    sc_canvas = Canvas( one, width = 500, height = 500, borderwidth = 2 )
+    sc_canvas.pack(side=RIGHT)
+
+    processList = []
+    selection = IntVar()
+    priority = StringVar()
+    timeslice = StringVar()
+    process = StringVar()
+    arrival = StringVar()
+    burst = StringVar()
+    
+   
+    def gantt(): 
+        summ = 0
+        if selection.get() == 3:  # shortest job first
+            for i in processList:
+                summ += int(i[2])
+            sc_canvas.create_rectangle(50, 150, summ * 10, 250, fill="blue")
+            sc_canvas.create_rectangle(50, 150, int(processList[0][2]) * 100, 250, fill="red")
+                
+       
+    def add():
+        t.insert(END,  processid.get() + "\t|\t" + arrivTime.get() + "\t|\t" + burstTime.get() + "\n")
+        processList.append(list((processid.get(), arrivTime.get(), burstTime.get())))
+
+    b = Button(left, text="Add Process", width=10, command=add)
+    g = Button(top, text="Produce Gantt Chart", width=10, command=gantt)
+    
+    priorityfield = Entry(left, textvariable = priority)
+    timequanta = Entry(left, textvariable = timeslice)
+    processid = Entry(left, textvariable = process)
+    arrivTime = Entry(left, textvariable = arrival)
+    burstTime = Entry(left, textvariable = burst)
+
+    Radiobutton(top, text = "Round Robin", variable = selection, value = 1).pack(side=LEFT)
+    Radiobutton(top, text = "Priority", variable = selection, value = 2).pack(side=LEFT)
+    Radiobutton(top, text = "SJF", variable = selection, value = 3).pack(side=LEFT)
+    g.pack(anchor=SE)
+
+    priorityLabel = Label(left, text="Priority (only for priority scheduling)").pack(anchor=NW)
+    priorityfield.pack(anchor = NW)
+    timeLabel = Label(left, text="Time slice (only for round robin)").pack(anchor=NW)
+    timequanta.pack(anchor = NW)
+    processLabel = Label(left, text="Process ID").pack(anchor=NW)
+    processid.pack(anchor = NW)
+    arrivalLabel = Label(left, text="Arrival Time").pack(anchor=W)
+    arrivTime.pack(anchor = W)
+    burstLabel = Label(left, text="Burst Time").pack(anchor=SW)
+    burstTime.pack(anchor = SW)
     b.pack()
-    add_b = Button( one, text = "Add", command = test )
-    add_b.pack()
-    clear_b = Button( one, text = "Clear", command = test )
-    clear_b.pack()
-    generate_b = Button( one, text = "Generate", command = test )
-    generate_b.pack()
+
+    t = Text(left, height = 20, width = 45)
+    t.pack()
+
+    t.insert(END, "Process ID \t|\tArrival Time\t|\tBurst Time\n")
     ##Add radio buttons to select a scheduler type
 
 # Memory Management Unit tab
@@ -105,9 +163,12 @@ def frame3():
         pr_alg_b.pack( side = LEFT )
 
     # generate button to submit the page replacement information
-    generate_b = Button( three, text = "Generate", command = test )
-    generate_b.pack()
+    pr_generate_b = Button( three, text = "Generate", command = pr_generate )
+    pr_generate_b.pack()
 
+    # the canvas where the page replacement results will be laid
+    pr_canvas = Canvas( three, width = 500, height = 500 )
+    pr_canvas.pack()
 
     '''
     string1 = []
@@ -133,15 +194,13 @@ def frame3():
     b3 = Button( cmds, text = "Random", command = randvar )
     b3.pack( side = LEFT )
     var = string1[0].get()'''
-    return string1, string2
+    return pr_generate_b, pr_canvas
 
 if __name__ == "__main__":
-    N = 5
-    M = 6
 
     root,one,two,three = gui()
-    frame1()
+    frame1(root)
     bcan, can = frame2()
-    string1, string2 = frame3()
+    pr_generate_b, pr_canvas = frame3()
 
     root.mainloop()
