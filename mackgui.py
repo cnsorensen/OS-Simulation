f35405f71401b01104a0b25ca91a299b87ea4c23
Smith <7057627@linux14.mcs.sdsmt.edu>
2016-05-04 20:23:20 -0600

    fucking done with this bullshit

--- mackgui.py
+++ mackgui.py
@@ -1,4 +1,4 @@
-
+from operator import *
 from Tkinter import *
 from ttk import *
 import random
@@ -65,7 +65,7 @@ def frame1(frame):
     left.pack(side=LEFT)
     top.pack(side=TOP)
 
-    sc_canvas = Canvas( one, width = 500, height = 500, borderwidth = 2 )
+    sc_canvas = Text( one, width = 75, height = 50 )
     sc_canvas.pack(side=RIGHT)
 
     processList = []
@@ -76,19 +76,47 @@ def frame1(frame):
     arrival = StringVar()
     burst = StringVar()
     
-   
     def gantt(): 
         summ = 0
+        ganttchart = ""  
+        
+
         if selection.get() == 3:  # shortest job first
             for i in processList:
-                summ += int(i[2])
-            sc_canvas.create_rectangle(50, 150, summ * 10, 250, fill="blue")
-            sc_canvas.create_rectangle(50, 150, int(processList[0][2]) * 100, 250, fill="red")
-                
-       
+                remainingTime.append(int(i[2]))
+            print remainingTime
+            for i in sorted(sorted(processList, key=itemgetter(1)), key=itemgetter(2)):
+                for j in range(0, int(i[2])):
+                    ganttchart += "| " + i[0] + " "
+
+
+        if selection.get() == 2: # priority scheduling
+            for i in sorted(processList, key=itemgetter(3)):
+                for j in range (0, int(i[2])):
+                    ganttchart += "| " + i[0] + " "
+
+        if selection.get() == 1: # round robin
+            totalslices = 0
+            remainingTime = []
+            for i in processList:
+                totalslices += int(i[2])
+            totalslices /= int(processList[0][4])
+            for i in processList:
+                if int(i[2]) > 0:
+                    for j in range (0, int(processList[0][4])):
+                        ganttchart += "| " + i[0] + " "
+                    next = int(i[2]) - 2
+                    if next < 0:
+                        next = 0
+                    processList.append(list((i[0], "fill", next)))
+
+        ganttchart += "\n\n\n"
+        sc_canvas.insert(END, ganttchart)
+
+
     def add():
         t.insert(END,  processid.get() + "\t|\t" + arrivTime.get() + "\t|\t" + burstTime.get() + "\n")
-        processList.append(list((processid.get(), arrivTime.get(), burstTime.get())))
+        processList.append(list((processid.get(), arrivTime.get(), burstTime.get(), priority.get(), timeslice.get())))
 
     b = Button(left, text="Add Process", width=10, command=add)
     g = Button(top, text="Produce Gantt Chart", width=10, command=gantt)
@@ -205,111 +233,3 @@ if __name__ == "__main__":
     pr_generate_b, pr_canvas = frame3()
 
     root.mainloop()
-
-from Tkinter import *
-from ttk import *
-import random
-
-
-def printvar(var):
-   print var[0].get()
-
-def pushvar(var):
-	for i in range(N-1,0,-1):
-		var[i].set(var[i-1].get())
-	var[0].set('')
-
-def line():
-	canline = can.create_line(0,0,200,100)
-	canbox = can.create_rectangle((150,150,250,250))
-	bcan.config(text="Hide", command=hide)
-
-def hide():
-	bcan.config(text="Draw",command=line)
-	can.delete("all")
-
-def randvar():
-	string2[0].set(random.randint(0,32))
-
-def gui():
-	#set up the window
-	root = Tk()
-	root.title("Sample GUI using Tkinter")
-	root.geometry("500x300")
-	#set up the tab structure
-	notebook = Notebook(root)
-
-	#set up each fo the tabs and add them 
-	one = Frame(notebook) 
-	two = Frame(notebook)
-	three = Frame(notebook) 
-
-	notebook.add(one, text = "Tab 1")
-	notebook.add(two, text = "Tab 2")
-	notebook.add(three, text = "Tab 3")
-	notebook.pack(side=TOP)
-	random.seed()
-	return root, one, two, three
-
-
-def frame1():
-	L = Label(one, text='A simple GUI')
-	L.pack()
-	b = Button(one, text="OK", command=exit)
-	b.pack()
-
-
-def frame2():
-	b = Button(two, text="Draw", command=line)
-	b.pack()
-	can = Canvas(two, width=300, height =300)
-	can.pack()
-	return b, can
-
-
-def frame3():
-	top = Frame(three)
-	top.pack(side=TOP)
-	bottom = Frame(three)
-	bottom.pack(side=TOP)
-	cmds = Frame(three)
-	cmds.pack(side=TOP)
-
-	string1 = []
-	entry1 = []
-	for i in range(M): 
-		string1.append(StringVar())
-		entry1.append(Entry(top, width=5, textvariable = string1[i]))
-		entry1[i].pack(side=LEFT)
-
-	string2 = []
-	entry2 = []
-	for i in range(N): 
-		string2.append(StringVar())
-		entry2.append(Entry(bottom, width=6, textvariable = string2[i]))
-		entry2[i].pack()
-
-
-	b1 = Button(cmds, text="Push", command=lambda: pushvar(string2))
-	b1.pack(side=LEFT)
-
-	b2 = Button(cmds, text="Print", command=lambda: printvar(string2))
-	b2.pack(side=LEFT)
-
-	b3 = Button(cmds, text="Random", command=randvar)
-	b3.pack(side=LEFT)
-	var = string1[0].get()
-	return string1, string2
-
-
-
-N = 5
-M = 6
-
-root,one,two,three = gui()
-frame1()
-bcan, can = frame2()
-string1, string2 = frame3()
-
-
-root.mainloop()
